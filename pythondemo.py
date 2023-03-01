91f438cc0cfe1cc03930eaf37e2814808973f1a1
abhijeetgitopeninsights <abhijeet@open-insights.com>
2023-03-01 12:43:13 +0530

    Another update Criteria

--- pythondemo.py
+++ pythondemo.py
@@ -3,5 +3,9 @@ print('hello world')
 A=input("are you happy to do work in that domain?")
 if A=="yes":
   print("graet! ,so let's start work")
+#test branch
+elif A== "Temporary":
+  print("no problem, you can do work in another project") 
 else:
-  print("no problem, you can do work in another project")  
+    print(" learn it another thing")
+    
