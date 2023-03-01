d70a2e6dbadae21bba4eefcc01c8fc709426e48e
abhijeetgitopeninsights <abhijeet@open-insights.com>
2023-03-01 12:27:51 +0530

    Uapadate program deatil

--- pythondemo.py
+++ pythondemo.py
@@ -1,3 +1,7 @@
 print('hello world')
 
-print('hello asif')
+A=input("are you happy to do work in that domain?")
+if A=="yes":
+  print("graet! ,so let's start work")
+else:
+  print("no problem, you can do work in another project")  
