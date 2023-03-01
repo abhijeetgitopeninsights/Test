8cd9b8c35f144de863cf63d78bf24aaf1554bee1
abhijeetgitopeninsights <abhijeet@open-insights.com>
2023-03-01 14:06:05 +0530

    Working message

--- pythondemo.py
+++ pythondemo.py
@@ -3,6 +3,7 @@ print('hello world')
 A=input("are you happy to do work in that domain?")
 if A=="yes":
   print("graet! ,so let's start work")
+  print("what you did now?")
 #test branch
 elif A== "Temporary":
   print("no problem, you can do work in another project") 
